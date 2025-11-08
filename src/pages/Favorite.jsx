bab489018afb9bc1100e8d3b7cf8c4c06fa92f67
Leshan <leshanpasindu12@gmail.com>
2025-11-08 10:30:03 +0530

    create favorite move

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Favorite = () => {
+  return (
+    <div className='favorites-empty'>
+      <h2>No favorites movies yet</h2>
+    </div>
+  )
+}
+
+export default Favorite
