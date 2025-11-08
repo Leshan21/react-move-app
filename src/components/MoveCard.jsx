bc99581ae5a772b9a35cd8305024e459049fcfcf
Leshan <leshanpasindu12@gmail.com>
2025-11-08 14:17:56 +0530

    connect with image link

--- src/components/MoveCard.jsx
+++ src/components/MoveCard.jsx
@@ -8,7 +8,7 @@ const MoveCard = ({ movie }) => {
   return (
     <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.backdrop_path} alt={movie.title} />   {/*Movie Post*/}
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />   {/*Movie Post*/}
             <div className="movie-overlay">
             <button className="favorite-btn" onClick={handleClick}>
             Click
