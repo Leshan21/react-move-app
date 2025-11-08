1a0b29813dfb5ea50246e132f486b04fb9680d69
Leshan <leshanpasindu12@gmail.com>
2025-11-08 08:56:34 +0530

    add data to props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import MoveCard from "./components/MoveCard";
 function App() {
   return (
     <>
-      <MoveCard />
+      <MoveCard
+        movie={{
+          title: "title here",
+          release_date: "2024-09-10",
+        }}
+      />
     </>
   );
 }
